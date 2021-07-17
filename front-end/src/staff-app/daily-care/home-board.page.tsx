6e2f88fb153b6e7726cd3fd8cc0fd3cd9210757a
nikhilreddy <nikhil.reddy@getmega.com>
2021-07-17 16:42:25 +0530

    search and sort implemented

--- front-end/src/staff-app/daily-care/home-board.page.tsx
+++ front-end/src/staff-app/daily-care/home-board.page.tsx
@@ -5,22 +5,51 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Spacing, BorderRadius, FontWeight } from "shared/styles/styles"
 import { Colors } from "shared/styles/colors"
 import { CenteredContainer } from "shared/components/centered-container/centered-container.component"
-import { Person } from "shared/models/person"
+import { Person, PersonHelper } from "shared/models/person"
 import { useApi } from "shared/hooks/use-api"
 import { StudentListTile } from "staff-app/components/student-list-tile/student-list-tile.component"
 import { ActiveRollOverlay, ActiveRollAction } from "staff-app/components/active-roll-overlay/active-roll-overlay.component"
+import { Filter } from "../../shared/models/filter"
 
 export const HomeBoardPage: React.FC = () => {
   const [isRollMode, setIsRollMode] = useState(false)
   const [getStudents, data, loadState] = useApi<{ students: Person[] }>({ url: "get-homeboard-students" })
+  const [students, setStudents] = useState<Person[]>([])
+  const [displayResults, setDisplayResults] = useState<Person[]>([])
+  const [filter, setFilter] = useState<Filter>({
+    first_name: "ASC",
+    last_name: null,
+    search: "",
+    roll: null
+  })
 
   useEffect(() => {
     void getStudents()
   }, [getStudents])
 
-  const onToolbarAction = (action: ToolbarAction) => {
+  useEffect(() => {
+    if(loadState === "loaded" && data?.students){
+      setStudents(data.students)
+      setDisplayResults(filterStudents(data.students))
+    }
+  }, [loadState])
+
+  useEffect(() => {
+    setDisplayResults(filterStudents(students))
+  }, [filter, isRollMode])
+
+  const onToolbarAction = (action: ToolbarAction, type: string, value: string) => {
     if (action === "roll") {
       setIsRollMode(true)
+    }
+    if(action === "sort"){
+      if(type === "search"){
+        setFilter({...filter, search: value})
+      } else if (type === "first"){
+        setFilter({...filter, first_name: filter.first_name === "ASC" ? "DESC" : "ASC", last_name: null})
+      } else if (type === "last"){
+        setFilter({...filter, last_name: filter.last_name === "ASC" ? "DESC" : "ASC", first_name: null})
+      }
     }
   }
 
@@ -30,20 +59,39 @@ export const HomeBoardPage: React.FC = () => {
     }
   }
 
+  const filterStudents = (list: Person[]) => {
+    const { first_name, last_name, search, roll} = filter
+    let filteredList = list;
+    if(search){
+      filteredList = filteredList.filter(a => PersonHelper.getFullName(a).toLowerCase().includes(search))
+    }
+    if(roll){
+      filteredList = filteredList.filter(a => a.roll === roll)
+    }
+    if(first_name){
+      filteredList.sort((a,b) => first_name === "ASC" ?
+        a.first_name.localeCompare(b.first_name) : b.first_name.localeCompare(a.first_name))
+    }
+    if(last_name){
+      filteredList.sort((a,b) => last_name === "ASC" ?
+        a.last_name.localeCompare(b.last_name) : b.last_name.localeCompare(a.last_name))
+    }
+    return filteredList
+  }
+
   return (
     <>
       <S.PageContainer>
-        <Toolbar onItemClick={onToolbarAction} />
+        <Toolbar onItemClick={onToolbarAction}  filter={filter}/>
 
         {loadState === "loading" && (
           <CenteredContainer>
             <FontAwesomeIcon icon="spinner" size="2x" spin />
           </CenteredContainer>
         )}
-
-        {loadState === "loaded" && data?.students && (
+        {loadState === "loaded" && (
           <>
-            {data.students.map((s) => (
+            {displayResults.map((s) => (
               <StudentListTile key={s.id} isRollMode={isRollMode} student={s} />
             ))}
           </>
@@ -62,14 +110,24 @@ export const HomeBoardPage: React.FC = () => {
 
 type ToolbarAction = "roll" | "sort"
 interface ToolbarProps {
-  onItemClick: (action: ToolbarAction, value?: string) => void
+  onItemClick: (action: ToolbarAction, type?: any, value?: any) => void
+  filter: Filter
 }
 const Toolbar: React.FC<ToolbarProps> = (props) => {
-  const { onItemClick } = props
+  const { onItemClick, filter } = props
   return (
     <S.ToolbarContainer>
-      <div onClick={() => onItemClick("sort")}>First Name</div>
-      <div>Search</div>
+      <div onClick={() => onItemClick("sort", "first")}>First Name&nbsp;
+        {filter?.first_name && (filter.first_name === "ASC"
+          ? <FontAwesomeIcon icon="sort-alpha-down" /> : <FontAwesomeIcon icon="sort-alpha-up" />)}
+      </div>
+      <div onClick={() => onItemClick("sort", "last")}>Last Name&nbsp;
+        {filter?.last_name && (filter.last_name === "ASC"
+          ? <FontAwesomeIcon icon="sort-alpha-down" /> : <FontAwesomeIcon icon="sort-alpha-up" />)}
+      </div>
+      <div>
+        <input onChange={(e)=> onItemClick("sort", "search", e?.target?.value)} value={filter.search}/>
+      </div>
       <S.Button onClick={() => onItemClick("roll")}>Start Roll</S.Button>
     </S.ToolbarContainer>
   )
